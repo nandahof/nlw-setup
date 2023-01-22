eaaa2e214bb67ad661b6f53b4f70b16e78b94350
Fernanda Hofstatter <nanda-hof@hotmail.com>
2023-01-22 14:07:32 -0300

    ajustes

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-const form = document.querySelector("form")
+const form = document.querySelector("#form-habits")
 const nlwSetup = new NLWSetup(form)
 const button = document.querySelector("header button")
 
